5623241e860cebea8c48339c5ed1602b6f37d152
ShubhSik <shubhamsikriwal@gmail.com>
2024-11-28 20:38:06 -0800

    Increased the timer to 5 min

--- src/main/java/edu/sdccd/cisc190/Main.java
+++ src/main/java/edu/sdccd/cisc190/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     private Label timerLabel;
     private VBox optionsBox;
     private int currentQuestionIndex = 0;
-    private int timeLeft = 30; // 30 seconds timer
+    private int timeLeft = 300; // 300 seconds timer
     private ScheduledExecutorService timerExecutor;
 
     public static void main(String[] args) {
